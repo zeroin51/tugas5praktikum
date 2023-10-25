5243119f9b973da1d689947e49eee2bd13e4944e
zeroin51 <davstwn88@gmail.com>
2023-10-25 22:10:14 +0700

    tugas prak 5

---
+++ lib/helpers/api_url.dart
@@ -0,0 +1,18 @@
+class ApiUrl {
+  static const String baseUrl = 'http://localhost/toko_api/public';
+  static const String registrasi = baseUrl + '/registrasi';
+  static const String login = baseUrl + '/login';
+  static const String listProduk = baseUrl + '/produk';
+  static const String createProduk = baseUrl + '/produk';
+  static String updateProduk(int id) {
+    return baseUrl + '/produk/' + id.toString() + '/update';
+  }
+
+  static String showProduk(int id) {
+    return baseUrl + '/produk/' + id.toString();
+  }
+
+  static String deleteProduk(int id) {
+    return baseUrl + '/produk/' + id.toString();
+  }
+}
---
+++ lib/model/produk.dart
@@ -0,0 +1,14 @@
+class Produk {
+  int? id;
+  String? kodeProduk;
+  String? namaProduk;
+  int? hargaProduk;
+  Produk({this.id, this.kodeProduk, this.namaProduk, this.hargaProduk});
+  factory Produk.fromJson(Map<String, dynamic> obj) {
+    return Produk(
+        id: int.parse(obj['id']),
+        kodeProduk: obj['kode_produk'],
+        namaProduk: obj['nama_produk'],
+        hargaProduk: int.parse(obj['harga']));
+  }
+}
